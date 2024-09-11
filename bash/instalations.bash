91f93b24d80fb48d1bf4dc43559ce4eedf342063
feraxhp <feraxhp+gh@gmail.com>
2024-09-10 23:17:15 -0500

    + alacritty installation

--- bash/instalations.bash
+++ bash/instalations.bash
@@ -24,3 +24,43 @@ install-minecraft(){
         echo "Done!"
     fi
 }
+
+install-alacritty() {
+    
+    echo "Preparations"
+    dir=$HOME/.temp/
+    mkdir -p $dir
+    cd $dir
+    git clone https://github.com/alacritty/alacritty.git
+    cd alacritty
+    
+    echo "installing dependencies..."
+    sudo dnf install -y cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++
+    
+    echo "Building allacrity"
+    cargo build --release
+    
+    echo "installing target"
+    sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+    
+    echo "adding manuals"
+    sudo mkdir -p /usr/local/share/man/man1
+    sudo mkdir -p /usr/local/share/man/man5
+    scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+    scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+    scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
+    scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+    
+    echo "Adding completitions"
+    mkdir -p ~/.bash_completion
+    cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
+    echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
+    
+    cd $HOME
+    rm -fr $dir
+    
+    echo "Done"
+}
