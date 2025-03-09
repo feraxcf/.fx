db3d1d5dff3b89e3a319dfd4965349d953d6f42c
feraxhp <feraxhp+gh@gmail.com>
2025-03-09 00:30:01 -0500

    ++ windows

--- bash/load.bash
+++ bash/load.bash
@@ -7,3 +7,13 @@ if uname -a | grep "microsoft" &> /dev/null; then
         done
     fi
 fi
+
+if uname -a | grep "MINGW64_NT" &> /dev/null; then 
+    if [ -d ~/.fx/bash/windows ]; then
+        for rc in -d ~/.fx/bash/windows/*.bash; do
+            if [ -f "$rc" ]; then
+                . "$rc"
+            fi
+        done
+    fi
+fi
---
+++ bash/windows/alias.bash
@@ -0,0 +1,1 @@
+alias sm='powershell.exe -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('\'^{ESC}\'')"'
