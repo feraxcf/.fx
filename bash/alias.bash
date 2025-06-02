505d80cd7904ccc7541ad364cd6080c4d2e59263
feraxhp <feraxhp+gh@gmail.com>
2025-06-01 23:00:09 -0500

    + bacon alias

--- bash/alias.bash
+++ bash/alias.bash
@@ -1,6 +1,10 @@
+
+# General aliases
 alias cl='clear'
 alias fx='cd ~/.fx'
 alias fxd='$HOME/.fx'
 alias x='exit 0'
 alias rl='source ~/.bashrc'
 
+# Rust-related aliases
+alias cw='bacon --headless run-long -- -q'
