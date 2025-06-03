2652f90d41ef53df9e2102c139fc8209f21a837d
feraxhp <feraxhp+gh@gmail.com>
2025-06-02 19:50:13 -0500

    * alias:pp|tp

--- bash/alias.bash
+++ bash/alias.bash
@@ -5,6 +5,8 @@ alias fx='cd ~/.fx'
 alias fxd='$HOME/.fx'
 alias x='exit 0'
 alias rl='source ~/.bashrc'
+alias pp='cd ~/projects/pprojects'
+alias tp='cd ~/projects/tprojects'
 
 # Rust-related aliases
 alias cw='bacon --headless run-long -- -q'
