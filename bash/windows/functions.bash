ab12822b54601d8187f955157b823841a4c4473a
feraxhp <feraxhp+gh@gmail.com>
2025-06-03 11:16:19 -0500

    * wp > u2w ++ wp: show windows paths

--- bash/windows/functions.bash
+++ bash/windows/functions.bash
@@ -1,7 +1,7 @@
 pws() { powershell.exe -Command "$@"; }
 cmd() { cmd.exe "/C $@"; }
 
-wp() {
+u2w() {
     if [ -t 0 ]; then
         # stdin is a terminal, so use argument
         input="$1"
@@ -11,4 +11,14 @@ wp() {
     fi
     output=$(echo "$input" | sed -E 's|^/([a-zA-Z])/|\1:\\|' | sed 's|/|\\|g')
     echo "$output"
+}
+
+wp() {
+    for arg in "$@"; do
+        local full_path=$(realpath "$arg" 2>/dev/null)
+        
+        if [ $? -eq 0 ] && [ -n "$full_path" ] && [ -e "$full_path" ]; then
+            u2w $full_path
+        fi
+    done
 }
