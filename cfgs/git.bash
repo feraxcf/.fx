976373ac676b2913ece81fae370be68e2aa84c21
feraxhp <feraxhp+gh@gmail.com>
2025-07-22 22:20:14 -0500

    *- jobs,
    *+ git config

---
+++ cfgs/git.bash
@@ -0,0 +1,7 @@
+echo -e "[user]\n	email = feraxhp+gh@gmail.com\n	name = feraxhp" >> ~/.gitconfig
+
+if command -v edit >/dev/null 2>&1; then
+    echo -e "[core]\n	editor = edit" >> ~/.gitconfig
+fi
+
+echo -e "[init]\n	defaultBranch = main" >> ~/.gitconfig
