19b42d00994edfe955cf29f9c6367834b24e2026
feraxhp <feraxhp+gh@gmail.com>
2025-11-12 08:35:27 -0500

    :improve non interative sessions

--- bash/_init.bash
+++ bash/_init.bash
@@ -1,27 +1,37 @@
 # by feraxhp <https://github.com/feraxhp>
 
-export current=$(realpath ~/.fx)
-
-for rc in -d $current/bash/config/*.bash; do
-    if [ -f "$rc" ]; then
-        . "$rc"
+init() {
+    export current=$(realpath ~/.fx)
+    
+    for rc in -d $current/bash/config/*.bash; do
+        if [ -f "$rc" ]; then
+            . "$rc"
+        fi
+    done
+    
+    . "$current/bash/_shell.bash"
+    
+    if [ -z "${ZED}" ] || [ "${ZED}" != "1" ]; then
+        echo -e "\e[36m"
+        echo "  ∕⁀⧵             "
+        echo "  \ ∕        ∕⁀⧵  "
+        echo "   X            | "
+        echo "  / \ ∕⁀⧵   |   | "
+        echo " |   |   ⧵‿∕ \ /  "
+        echo " |            X   "
+        echo "  ⧵‿∕        ∕ \  "
+        echo "             ⧵‿∕  "
     fi
-done
+    
+    unset rc
+    unset SSH_ASKPASS
+    unset current
+}
 
-. "$current/bash/_shell.bash"
-
-if [ -z "${ZED}" ] || [ "${ZED}" != "1" ]; then
-    echo -e "\e[36m"
-    echo "  ∕⁀⧵             "
-    echo "  \ ∕        ∕⁀⧵  "
-    echo "   X            | "
-    echo "  / \ ∕⁀⧵   |   | "
-    echo " |   |   ⧵‿∕ \ /  "
-    echo " |            X   "
-    echo "  ⧵‿∕        ∕ \  "
-    echo "             ⧵‿∕  "
+if [[ -n "$PS1" ]]; then
+    init
 fi
 
-unset rc
-unset SSH_ASKPASS
-unset current
+if [ "${ZED}" == "1" ]; then
+    init
+fi
