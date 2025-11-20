c8b48ee09f3ea4e8a0f659f5049eeba942bd7e5d
feraxhp <feraxhp+gh@gmail.com>
2025-11-19 22:24:28 -0500

    ++ -y

--- cfgs/dnf.bash
+++ cfgs/dnf.bash
@@ -16,11 +16,11 @@ echo "-> Installing grp"
 version_name=$(curl -s "https://api.github.com/repos/feraxhp/grp/releases/latest" | jq -r '.name')
 nversion=$(echo "$version_name" | cut -d'v' -f2)
 
-sudo dnf install "https://github.com/feraxhp/grp/releases/download/$version_name/grp-$nversion-1.x86_64.rpm"
+sudo dnf install -y "https://github.com/feraxhp/grp/releases/download/$version_name/grp-$nversion-1.x86_64.rpm"
 
 
 echo "-> Installing cim"
 version_name=$(curl -s "https://api.github.com/repos/feraxhp/cim/releases/latest" | jq -r '.name')
 nversion=$(echo "$version_name" | cut -d'v' -f2)
 
-sudo dnf install "https://github.com/feraxhp/cim/releases/download/$version_name/cim-$nversion-1.x86_64.rpm"
+sudo dnf install -y "https://github.com/feraxhp/cim/releases/download/$version_name/cim-$nversion-1.x86_64.rpm"
