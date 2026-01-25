3b0cde682decedda23478911a009193fe9e3a855
feraxhp <feraxhp+gh@gmail.com>
2026-01-25 12:13:48 -0500

    :chmod +x hopp.agent

--- apps/desktop/hopp.sh
+++ apps/desktop/hopp.sh
@@ -2,12 +2,14 @@
 
 config() {
     local apps="$HOME/apps/"
+    local target="$apps/hopp.agent"
     local start="$HOME/.config/autostart"
     
     mkdir -p "$apps" "$start"
     echo "" > "$start/Hoppscotch Agent.desktop"
     
-    curl -L "https://github.com/hoppscotch/agent-releases/releases/latest/download/Hoppscotch_Agent_linux_x64.AppImage" -o "$apps/hopp.agent"
+    curl -L "https://github.com/hoppscotch/agent-releases/releases/latest/download/Hoppscotch_Agent_linux_x64.AppImage" -o "$target"
+    chmod +x "$target"
 }
 
 config
