5ac8e2182fce872401382f59932c41f75fb4f116
feraxhp <feraxhp+gh@gmail.com>
2026-02-13 09:40:19 -0500

    add: config --root

--- cfgs/bashrc.bash
+++ cfgs/bashrc.bash
@@ -1,2 +1,20 @@
+#!/usr/bin/env bash
 
-echo -e "\nfile=\"\$HOME/.fx/bash/_init.bash\"\nif [ -f \$file ]; then . \$file; fi" >> $HOME/.bashrc
+config() {
+    local home=$HOME
+    local target=$HOME
+    local main="\nfile=\"\$HOME/.fx/bash/_init.bash\"\nif [ -f \$file ]; then . \$file; fi"
+    
+    [[ "$1" == "--root" ]] && {
+        target="/root"
+        
+        echo "export HOME=$home" | sudo tee -a "$target/.bashrc" > /dev/null 
+        echo -e "$main"  | sudo tee -a "$target/.bashrc" > /dev/null
+        echo "export HOME=/root" | sudo tee -a "$target/.bashrc" > /dev/null
+    
+    } || {
+        echo -e "$main" >> $target/.bashrc
+    }
+}
+
+config "$@"
